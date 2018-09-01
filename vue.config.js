7d10dde795a66ac1f85c343a8f3c452c2220af93
sunjiancn <sunjian.c@qq.com>
2018-09-01 17:27:06 +0800

    init

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  runtimeCompiler: true,
+  lintOnSave: undefined,
+  baseUrl: undefined,
+  outputDir: undefined,
+  assetsDir: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+
+  css: {
+    modules: true,
+    sourceMap: true
+  }
+}
